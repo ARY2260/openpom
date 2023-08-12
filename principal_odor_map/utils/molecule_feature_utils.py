4cca47d79027a6181723c4acc21e1bec198bf7d9
Aryan Amit Barsainyan <arb.aryan@gmail.com>
2023-08-12 15:42:45 +0530

    add valence feat utils

--- principal_odor_map/utils/molecule_feature_utils.py
+++ principal_odor_map/utils/molecule_feature_utils.py
@@ -31,3 +31,33 @@ def get_atomic_num_one_hot(atom: RDKitAtom,
     """
     return one_hot_encode(atom.GetAtomicNum() - 1, allowable_set,
                           include_unknown_set)
+
+
+def get_atom_total_valence_one_hot(
+        atom: RDKitAtom,
+        allowable_set: List[int],
+        include_unknown_set: bool = True) -> List[float]:
+    """Get an one-hot feature for total valence of an atom.
+
+    Parameters
+    ---------
+    atom: rdkit.Chem.rdchem.Atom
+        RDKit atom object
+    allowable_set: List[int]
+        Atom total valence to consider.
+    include_unknown_set: bool, default True
+        If true, the index of all types not in
+        `allowable_set` is `len(allowable_set)`.
+
+    Returns
+    -------
+    List[float]
+        A one-hot vector for total valence an atom has.
+        If `include_unknown_set` is False, the length is
+        `len(allowable_set)`.
+        If `include_unknown_set` is True, the length is
+        `len(allowable_set) + 1`.
+
+    """
+    return one_hot_encode(atom.GetTotalValence(), allowable_set,
+                          include_unknown_set)
