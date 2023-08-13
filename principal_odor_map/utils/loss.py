3f94ae00d2c98733d993e86ff7eae5f9098e7ba5
Aryan Amit Barsainyan <arb.aryan@gmail.com>
2023-08-13 12:43:29 +0530

    add loss

--- principal_odor_map/utils/loss.py
+++ principal_odor_map/utils/loss.py
@@ -1,5 +1,5 @@
 import torch
-from typing import List, Optional, Callable, Any
+from typing import Optional, Callable
 from deepchem.models.losses import Loss
 
 
@@ -7,40 +7,77 @@ class CustomMultiLabelLoss(Loss):
     """
     Custom Multi-Label Loss function for multi-label classification.
 
-    The cross entropy between two probability distributions.
+    The objective function is a summed cross-entropy loss over all tasks,
+    with each tasks's contribution to the loss being weighted by a factor
+    of log(1+ class_imbalance_ratio), such that rarer tasks were given
+    a higher weighting.
+
+    This loss function is based on:
+    `A Principal Odor Map Unifies Diverse Tasks in Human Olfactory Perception
+    preprint <https://www.biorxiv.org/content/10.1101/2022.09.01.504602v4>`_.
 
     The labels should have shape (batch_size) or (batch_size, tasks), and be
     integer class labels.  The outputs have shape (batch_size, classes) or
-    (batch_size, tasks, classes) and be logits that are converted to probabilities
-    using a softmax function.
+    (batch_size, tasks, classes) and be logits that are converted to
+    probabilities using a softmax function.
     """
-    def __init__(self,
-                 class_imbalance_ratio: Optional[torch.Tensor] = None,
-                 loss_aggr_type: str = 'sum',
-                 ):
+
+    def __init__(
+        self,
+        class_imbalance_ratio: Optional[torch.Tensor] = None,
+        loss_aggr_type: str = 'sum',
+    ):
         """
+        Parameters
+        ---------
+        class_imbalance_ratio: Optional[torch.Tensor]
+            Tensor of class imbalance ratios
+        loss_aggr_type: str
+            loss aggregation type; 'sum' or 'mean'
         """
         super(CustomMultiLabelLoss, self).__init__()
         if class_imbalance_ratio is None:
-            raise Warning("No class imbalance ratio provided!")
+            print(Warning("No class imbalance ratio provided!"))
         else:
             if not isinstance(class_imbalance_ratio, torch.Tensor):
-                raise Exception('class imbalance ratio should be a torch.Tensor')
-        self.class_imbalance_ratio: Optional[torch.Tensor] = class_imbalance_ratio
-        
+                raise Exception(
+                    'class imbalance ratio should be a torch.Tensor')
+        self.class_imbalance_ratio: Optional[
+            torch.Tensor] = class_imbalance_ratio
+
         if loss_aggr_type not in ['sum', 'mean']:
             raise Exception(f"Invalid loss aggregate type: {loss_aggr_type}")
         self.loss_aggr_type: str = loss_aggr_type
 
-    def _create_pytorch_loss(self) -> Callable:
+    def _create_pytorch_loss(
+            self) -> Callable[[torch.Tensor, torch.Tensor], torch.Tensor]:
         """
+        Returns loss function for pytorch backend
         """
-        ce_loss_fn: torch.nn.CrossEntropyLoss = torch.nn.CrossEntropyLoss(reduction='none')
+        ce_loss_fn: torch.nn.CrossEntropyLoss = torch.nn.CrossEntropyLoss(
+            reduction='none')
 
         def loss(output: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
             """
+            The objective function is a summed cross-entropy loss over all
+            tasks, with each tasks's contribution to the loss being weighted
+            by a factor of log(1+ class_imbalance_ratio), such that rarer
+            tasks were given a higher weighting.
+
+            Parameters
+            ---------
+            output: torch.Tensor
+                Output logits from model's forward pass per batch
+            labels: torch.Tensor
+                Target labels per batch
+
+            Returns
+            -------
+            loss: torch.Tensor
+                total or mean loss depending on loss aggregation type
             """
-            # Convert (batch_size, tasks, classes) to (batch_size, classes, tasks)
+            # Convert (batch_size, tasks, classes)
+            # to (batch_size, classes, tasks)
             # CrossEntropyLoss only supports (batch_size, classes, tasks)
             # This is for API consistency
             if len(output.shape) == 3:
@@ -51,28 +88,44 @@ class CustomMultiLabelLoss(Loss):
 
             # handle multilabel
             # output shape => (batch_size, classes=1, tasks)
-            # binary_output shape => (batch_size, classes=2, tasks) where now we have (1 - probabilities) for ce loss calculation
+            # binary_output shape => (batch_size, classes=2, tasks)
+            # where now we have (1 - probabilities) for ce loss calculation
             probabilities: torch.Tensor = output[:, 0, :]
             complement_probabilities: torch.Tensor = 1 - probabilities
-            binary_output: torch.Tensor = torch.stack([complement_probabilities, probabilities], axis=1)
+            binary_output: torch.Tensor = torch.stack(
+                [complement_probabilities, probabilities], dim=1)
 
             ce_loss: torch.Tensor = ce_loss_fn(binary_output, labels.long())
 
             if self.class_imbalance_ratio is None:
-                total_loss: torch.Tensor = ce_loss.sum()
-                return total_loss
+                if self.loss_aggr_type == 'sum':
+                    loss: torch.Tensor = ce_loss.sum(dim=1)
+                else:
+                    loss = ce_loss.mean(dim=1)
             else:
                 if len(self.class_imbalance_ratio) != ce_loss.shape[1]:
-                    raise Exception("size of class_imbalance_ratio should be equal to n_tasks")
-                balancing_factors: torch.Tensor = torch.log(1 + self.class_imbalance_ratio)
-                balanced_losses: torch.Tensor = torch.mul(ce_loss, balancing_factors) # loss being weighted by a factor of log(1+ class_imbalance_ratio)
+                    raise Exception("size of class_imbalance_ratio \
+                            should be equal to n_tasks")
+                balancing_factors: torch.Tensor = torch.log(
+                    1 + self.class_imbalance_ratio)
+
+                # loss being weighted by a factor of
+                # log(1+ class_imbalance_ratio)
+                balanced_losses: torch.Tensor = torch.mul(
+                    ce_loss, balancing_factors)
 
                 if self.loss_aggr_type == 'sum':
-                    total_loss = balanced_losses.sum(axis=1) # sum balanced loss across all tasks; shape => (batch_size)
-
-                    # duplicate loss across all tasks in a batch; shape => (batch_size, n_tasks)
-                    # This is for API consistency
-                    return total_loss.unsqueeze(-1).repeat(1,output.shape[-1])
+                    # sum balanced loss across all tasks;
+                    # shape => (batch_size)
+                    loss = balanced_losses.sum(dim=1)
                 else:
-                    return balanced_losses
+                    # mean balanced loss across all tasks;
+                    # shape => (batch_size)
+                    loss = balanced_losses.mean(dim=1)
+
+            # duplicate loss across all tasks in a batch;
+            # shape => (batch_size, n_tasks)
+            # This is for API consistency
+            return loss.unsqueeze(-1).repeat(1, output.shape[-1])
+
         return loss
