51ab72ed3f4889f6b47c6f11b67602d129ab3751
Aryan Amit Barsainyan <arb.aryan@gmail.com>
2023-08-20 19:33:19 +0530

    ADD base config

---
+++ principal_odor_map/hyper/test/test_base_config.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+from principal_odor_map.hyper.configs.config import Config
+
+
+def test_config():
+    """
+    """
+    np.random.seed(0)
+    Config.PARAMS_DICT = {
+        "param1": ["p1v1", "p1v2"],
+        "param2": ["p2v1", "p2v2", "p2v3"]
+    }
+    n_trials = 2
+    param_dict, path = Config.generate_hyperparams_random(
+        n_trials=n_trials, dir="principal_odor_map/hyper/test/assets")
+    assert isinstance(param_dict, dict)
+    assert isinstance(path, str)
+    assert len(param_dict.keys()) == n_trials
+    for _, params in param_dict.items():
+        assert "param1" in params
+        assert "param2" in params
+
+    # remove saved file
+    os.remove(path)
