5434c4b7badebca8e7d180aa34d2f787402121ee
Aryan Amit Barsainyan <75680850+ARY2260@users.noreply.github.com>
2025-10-17 20:19:50 +0530

    Update requirements in setup.py
    Replaced dynamic requirements loading with static list.

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
-# Read the requirements from the requirements.txt file
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+requirements = ['pyrfume',
+                'dgllife',
+                'pytest',
+                'ipykernel',
+                'scikit-multilearn']
 
 setup(
     name='openpom',
